f1ed449a3170ddb2523b5c7c18229d49b2203d05
abeni-al7 <abenezeralebachew3@gmail.com>
2025-01-08 23:32:50 +0300

    Add permissions

--- complaints/views.py
+++ complaints/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from .models import Complaint
 from .serializers import ComplaintSerializer
@@ -7,3 +8,4 @@ from .serializers import ComplaintSerializer
 class ComplaintViewSet(viewsets.ModelViewSet):
     queryset = Complaint.objects.all()
     serializer_class = ComplaintSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- routes/views.py
+++ routes/views.py
@@ -1,8 +1,10 @@
 from .models import Route
 from .serializers import RouteSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+    permission_classes = [permissions.IsAuthenticated]
